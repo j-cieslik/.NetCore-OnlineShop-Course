2ddfd2f0f5f716c27d504b57218a2038a010c085
Kuba <jciesmail@gmail.com>
2020-12-13 15:36:30 +0100

    Add nav-bar

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -5,12 +5,12 @@ using Core.Specification;
 
 namespace Core.Interfaces
 {
-    public interface IGenericRepository<T> where T : BaseEntity
-    {
-        Task<T> GetByIdAsync(int id);
-        Task<IReadOnlyList<T>> ListAllAsync();
-        Task<T> GetEntityWithSpec(ISpecification<T> spec);
-        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
-        Task<int> CountAsync(ISpecification<T> spec);
-    }
+  public interface IGenericRepository<T> where T : BaseEntity
+  {
+    Task<T> GetByIdAsync(int id);
+    Task<IReadOnlyList<T>> ListAllAsync();
+    Task<T> GetEntityWithSpec(ISpecification<T> spec);
+    Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+    Task<int> CountAsync(ISpecification<T> spec);
+  }
 }
